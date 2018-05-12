e5e320599431f3fb647a908d93a7b0fb0815e277
Kamil Pituła <kamil.pitula@outlook.com>
2018-05-12 13:20:18 +0200

    write to file added

--- reader.py
+++ reader.py
@@ -6,7 +6,7 @@ print("Starting reader press CTRL + C to exit")
 clockPin = 4
 dataInputPins = [5,6,13,19,26,16,20,21]
 
-timeSleep = 1;
+timeSleep = 0.001;
 
 GPIO.setmode(GPIO.BCM)
 mode = GPIO.getmode()
@@ -19,16 +19,20 @@ running = True
 counter = 0
 
 try:
-    while counter < 5:
+    f = open("output.txt","w")
+    while counter < 1000:
         GPIO.output(clockPin, GPIO.HIGH)
         time.sleep(timeSleep)
         value = 0
         for i in range(0, 8):
             value += GPIO.input(dataInputPins[i]) << i
-        print("Value from GPIO: {0}".format(str(value)))
+        character = str(chr(value))
+        print("Value of sample {0} from GPIO: {1}".format(counter,character))
+        f.write(character)
         GPIO.output(clockPin, GPIO.LOW)
         time.sleep(timeSleep)
         counter += 1
+    f.close()
 
 except KeyboardInterrupt:
     print("Keyboard interrupt")
